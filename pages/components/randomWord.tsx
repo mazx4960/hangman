17357d6973ebaa220f524f2ffe9a758659f2d770
Coco <chuaziyifaith@gmail.com>
2022-05-26 14:17:42 +0800

    amended random word generator
    added english.txt for fun

--- pages/components/randomWord.tsx
+++ pages/components/randomWord.tsx
@@ -1,20 +1,33 @@
-const WORDS = [
-  "plan",
-  "abounding",
-  "monkey",
-  "test",
-  "spell",
-  "dare",
-  "rebel",
-  "stocking",
-  "wacky",
-  "improve",
-  "colossal",
-  "open"
-];
+import React, { useState, useEffect } from 'react';
 
 function randomWord() {
-  return WORDS[Math.floor(Math.random() * WORDS.length)];
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://random-word-api.herokuapp.com/word")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return "Error";
+  } else if (!isLoaded) {
+    return "Loading";
+  } else {
+    return items;
+  }
+
 }
 
 export { randomWord };
